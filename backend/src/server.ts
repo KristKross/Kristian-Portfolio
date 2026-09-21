bf3dc23432db258e34a922a1cdf50df4b5d2d428
Kristian Demonteverde <kristkross30@gmail.com>
2026-09-21 20:16:30 +0400

    added change

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -31,5 +31,5 @@ app.use("/api/skills", skillRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port http://localhost/${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
